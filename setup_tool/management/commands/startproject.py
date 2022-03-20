88d4e7eef9c1c252cad7dfd102d973d19b797adc
cafitac <mwkang@humelo.com>
2022-03-21 05:13:56 +0900

    feat: Set startproject template

--- setup_tool/management/commands/startproject.py
+++ setup_tool/management/commands/startproject.py
@@ -17,4 +17,6 @@ class Command(TemplateCommand):
         # Create a random SECRET_KEY to put it in the main settings.
         options["secret_key"] = SECRET_KEY_INSECURE_PREFIX + get_random_secret_key()
 
+        options["template"] = './setup_tool/project_template'
+
         super().handle("project", project_name, target, **options)
