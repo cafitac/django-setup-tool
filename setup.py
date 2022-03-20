1b0b98a137bf9e146ebe029ccdfd5a4a29563a8d
cafitac <mwkang@humelo.com>
2022-03-21 04:44:40 +0900

    fix: Fix typo error

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent" "Programming Language :: Python",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
