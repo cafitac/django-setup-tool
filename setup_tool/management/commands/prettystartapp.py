cb2166e04507e302b761baced598a737dd90d343
cafitac <mwkang@humelo.com>
2022-03-21 11:36:32 +0900

    refactor: Change startapp file name to prettystartapp

--- setup_tool/management/commands/prettystartapp.py
+++ setup_tool/management/commands/prettystartapp.py
@@ -11,4 +11,5 @@ class Command(TemplateCommand):
     def handle(self, **options):
         app_name = options.pop("name")
         target = options.pop("directory")
+
         super().handle("app", app_name, target, **options)
