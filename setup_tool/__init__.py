acb6110eed56d517e94367de11a8ca1cd9cfb284
cafitac <mwkang@humelo.com>
2022-03-21 13:16:07 +0900

    chore: Update package version to 0.0.8

--- setup_tool/__init__.py
+++ setup_tool/__init__.py
@@ -3,7 +3,7 @@ setup_tool package information
 """
 
 __title__ = "Django Setup Tool"
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __author__ = "Minwoo Kang"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022 Minwoo Kang"
